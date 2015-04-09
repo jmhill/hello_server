9492aaecca7d7c7339fa419f706e7e4f818df2a9
Justin Hill <justinmhill@gmail.com>
2015-04-09 17:47:42 -0400

    return version and return specific headerValue when request url is headers/headerValue

--- hello_server.js
+++ hello_server.js
@@ -1,9 +1,17 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
+  response.setHeader('Content-Type', 'application/json');	
+
   if (request.url == '/headers') {
-    response.setHeader('Content-Type', 'application/json');	
     response.write(JSON.stringify(request.headers));
+  } else if (request.url == '/version') {
+  	response.write("The HTTP version of the request: " + request.httpVersion);
+  } else {
+  	var urlArray = request.url.split('/');
+  	var headerKey = urlArray[2];
+  	var headerValue = request.headers[headerKey];
+  	response.write(headerKey + ": " + headerValue);
   }
   
   response.end();
